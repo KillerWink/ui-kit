3bc9ecf31f47680dd4c238570c981eb475b035e9
thomasgambina <thomas.gambina@gmail.com>
2020-03-27 22:41:18 +0100

    change colour object key

--- packages/Theme/colors.js
+++ packages/Theme/colors.js
@@ -1,9 +1,9 @@
 const colors = {
-    'color-1': '#468cbb',
-    'color-2': '#81d2f2',
-    'color-3': '#fedfa0',
-    'color-4': '#f99799',
-    'color-5': '#FFFFFF'
+    color1: '#468cbb',
+    color2: '#81d2f2',
+    color3: '#fedfa0',
+    color4: '#f99799',
+    color5: '#FFFFFF'
 };
 
 export default colors;
